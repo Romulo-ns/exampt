8cf909836c611c6e8552c7111820b1a1b53a3e62
Romulo-ns <romulo.na.santos@gmail.com>
2026-04-28 17:39:54 +0100

    fix: force include prisma dependencies and standardize build structure

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import '@prisma/adapter-pg';
+import 'pg';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
